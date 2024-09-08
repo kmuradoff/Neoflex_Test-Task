7914f9a13461571dac3368417993ef504c4c26ab
Kamal Muradov <muradoffk@gmail.com>
2024-09-08 15:34:35 +0300

    Init commit ( Spring 2.7.5 )

---
+++ src/main/java/ru/neoflex/neoflexdemo/NeoflexDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.neoflex.neoflexdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoflexDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeoflexDemoApplication.class, args);
+    }
+
+}
